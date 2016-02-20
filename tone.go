f3b9ca7903feb168bc04e2a05bceb8e06f01d8e2
Max <electricerger@gmail.com>
2016-02-20 15:04:39 -0500

    Tone generator function complete

--- tone.go
+++ tone.go
@@ -1,4 +1,6 @@
 package main
+import("fmt", "math")
+
 
 type ToneGenerator struct {
 	sampleRate float64
@@ -15,6 +17,12 @@ func NewToneGenerator(sampleRate float64) *Tone {
 	return t
 }
 
+//Max's function - Given freq and delta t return an array for each step
+//Call Tone with freq and sec (floats) - return array
 func (t *ToneGenerator) Tone(freq, seconds float64) []int32{
-	return nil
+	var synthArray = make([]int32, int(duration*sampleRate)) //duration/step = dur*sR
+	for i:=0; i < len(synthArray); i++{
+		synthArray[i] = sin(freq * math.PI * i * step)
+	}
+	return synthArray
 }
