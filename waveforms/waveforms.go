bf76cae5bce335ecf6a04deb16e5c3a8061f9096
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2016-02-20 18:53:59 -0500

    Added waveforms and added *~ .swp to .gitignore

---
+++ waveforms/waveforms.go
@@ -0,0 +1,17 @@
+package waveforms
+import ("math")
+
+func Sin(x float64) float64 {
+	xMod:= math.Remainder(x,1)
+	return math.Sin(math.Pi*2*xMod)
+}
+
+func Tri(x float64) float64 {
+	xMod:= math.Remainder(x,1)
+	if xMod<0.5 {
+  	return 1. - xMod*4.
+  }
+  else {
+  	return (xMod*4. - 3.)
+  }
+}
