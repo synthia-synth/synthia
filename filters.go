10f1f6e506c2f4666439908b6723b5e633cc655c
Michael Williams <michael@mgwsoftware.com>
2016-02-21 00:45:55 -0500

    Fix filter.go

--- filters.go
+++ filters.go
@@ -15,24 +15,24 @@ func adsr1 (signal []int32) []int32{
 	var stoppingPoint = riseSamples
 	var curve = 0.0
 
-	for i=0; i<stoppingPoint; i++{ //Rise (lin)
-		curve = float32(i)/float32(riseSamples)
+	for i:=0; i<stoppingPoint; i++{ //Rise (lin)
+		curve = float64(i)/float64(riseSamples)
 		signal[i] = signal[i] * int32(curve)
 	}
-	stoppingPoint = riseSamples + stoppingPointdecaySamples
-	var lambda = float32(decaySamples)/Math.Ln(sustainLvl)
-	for i=riseSamples; i<stoppingPoint; i++{ //Decay (exp)
-		curve = Math.Exp(float32(i-riseSamples+1)/lambda)
+	stoppingPoint = riseSamples + decaySamples
+	var lambda = float64(decaySamples)/math.Log(sustainLvl)
+	for i:=riseSamples; i<stoppingPoint; i++{ //Decay (exp)
+		curve = math.Exp(float64(i-riseSamples+1)/lambda)
 		signal[i] = signal[i] * int32(curve)
 	}
 	stoppingPoint = len(signal)-releaseSamples
-	for i=riseSamples+decaySamples; i<stoppingPoint; i++{ //Sustain (const)
-		signal[i] = signal[i] * sustainLvl
+	for i:=riseSamples+decaySamples; i<stoppingPoint; i++{ //Sustain (const)
+		signal[i] = int32(float64(signal[i]) * sustainLvl)
 	}
 	stoppingPoint = len(signal)
-	lambda = float32(releaseSamples)/Math.Ln(sustainLvl)
-	for i=len(singal)-releaseSamples; i<stoppingPoint; i++{ //Release (exp)
-		curve = sustainLvl*Math.Exp(float32(i-len(signal)-releaseSamples+1)/lambda)
+	lambda = float64(releaseSamples)/math.Log(sustainLvl)
+	for i:=len(signal)-releaseSamples; i<stoppingPoint; i++{ //Release (exp)
+		curve = sustainLvl*math.Exp(float64(i-len(signal)-releaseSamples+1)/lambda)
 		signal[i] = signal[i] * int32(curve)
 	}
 
